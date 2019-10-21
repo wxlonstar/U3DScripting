0f8f3e8b10df22c3a72a85435c11874660ba662f
zhulei <zhulei@xmfunny.com>
2019-10-21 18:23:17 +0800

    Editor001

---
+++ Assets/Scripts/Test/Editor001.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class Editor001 {
+    [MenuItem("Help/Create Editor001")]
+    private static void CreateEditor001() {
+        if (EditorUtility.DisplayDialog(
+                "This is Title",
+                "This is message",
+                "ComfirmButton",
+                "CancelButton"
+            )) {
+            GameObject wazzup =  new GameObject("Mankind.");        // A new Gameobject named mankind will be generated.
+            Debug.Log(wazzup);
+            wazzup.tag = "Player";      // Can't add non - exsiting tag
+         
+
+        }
+    }
+}
