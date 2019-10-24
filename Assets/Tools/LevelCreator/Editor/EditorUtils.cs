8bda78564a9d9468acaaeb95344e8ca8201e0fd0
zhulei <zhulei@xmfunny.com>
2019-10-24 20:36:41 +0800

    SceneManagerUsage

---
+++ Assets/Tools/LevelCreator/Editor/EditorUtils.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+
+namespace RunRun.LevelCreator {
+    public static class EditorUtils {
+        // check if current scene needs to be saved.
+        public static void CreateNewScene() {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+        }
+
+        // remove every gameobject in the scene
+        public static void CleanScene() {
+            GameObject[] allObjs = Object.FindObjectsOfType<GameObject>();
+            foreach(GameObject obj in allObjs) {
+                GameObject.DestroyImmediate(obj);
+            }
+        }
+
+        // actual method needs to be called.
+        public static void NewLevel() {
+            CreateNewScene();
+            CleanScene();
+            GameObject levelInScene = new GameObject("Level");
+            levelInScene.transform.position = Vector3.zero;
+            levelInScene.AddComponent<Level>();
+        }
+    }
+}
---
+++ Assets/Tools/LevelCreator/Editor/MyMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+//I create all the V here
+namespace RunRun.LevelCreator {
+    public static class MyMenu {
+        [MenuItem("Tools/New Scene")]
+        private static void CreateNewLevel() {
+            EditorUtils.NewLevel();
+        }
+        
+    }
+
+   
+}
