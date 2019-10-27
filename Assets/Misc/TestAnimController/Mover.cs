d0c40fa5cdbaef0cb2cb14f851f99df0f558aa29
zhulei <zhulei@xmfunny.com>
2019-10-27 23:54:06 +0800

    add UnityChan Demo

--- Assets/Misc/TestAnimController/Mover.cs
+++ Assets/Misc/TestAnimController/Mover.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour {
+    private Transform myPlayer = null;
+    public float speed = 1f;
     // Start is called before the first frame update
-    void Start() {
-        
+    void Awake() {
+        myPlayer = this.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        myPlayer.transform.position += speed * myPlayer.transform.forward * Time.deltaTime;
     }
 }
