256b4f0322d6e7c482cece2d618d0d6490431764
zhulei <zhulei@xmfunny.com>
2019-10-28 03:33:52 +0800

    update

---
+++ Assets/Misc/TestAnimController/UseCC.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseCC : MonoBehaviour {
+    private CharacterController cc = null;
+    private 
+    // Start is called before the first frame update
+    void Awake() {
+        cc = this.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        cc.Move(this.transform.forward * Time.deltaTime);
+    }
+}
