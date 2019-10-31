fbccedd27327fad08db7cbbf7167329bcba9474a
zhulei <zhulei@xmfunny.com>
2019-11-01 01:37:07 +0800

    TestMatrix

---
+++ Assets/Scripts/Test/TestMatrix.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMatrix : MonoBehaviour {
+
+    public Transform trans;
+    
+    private void OnDrawGizmos() {
+
+
+        Debug.Log("localPosition: " + this.transform.localPosition);
+        Vector3 posWorld = this.transform.root.localToWorldMatrix.MultiplyPoint(this.transform.localPosition);
+        Debug.Log("worldPosition: " + posWorld);
+        Vector3 posCam = trans.worldToLocalMatrix.MultiplyPoint(posWorld);
+        Debug.Log("CameraPosition: " + posCam);
+    }
+
+}
