0db1dba4163812f1581f4edda0dc28071caa557d
朱磊 <zhulei@xmfunny.com>
2019-11-05 19:00:15 +0800

    keep updating

---
+++ Assets/Scripts/Test/UsingEventType.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingEventType : MonoBehaviour {
+    private Event e;
+   
+    // Start is called before the first frame update
+    void Start() {
+       
+    }
+
+    // Update is called once per frame
+    private void OnGUI() {
+        e = Event.current;
+        if(e.type == EventType.MouseUp) {
+            Debug.Log(e.mousePosition);
+        }
+    }
+}
