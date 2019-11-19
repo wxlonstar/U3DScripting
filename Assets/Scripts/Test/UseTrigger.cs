69c6aa17c44b4238eb3f51b284e6d7352a567e2c
朱磊 <zhulei@xmfunny.com>
2019-11-19 14:22:01 +0800

    Try Trigger

---
+++ Assets/Scripts/Test/UseTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseTrigger : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("KK");
+        Debug.Log(other.gameObject.ToString());
+        other.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+    }
+   
+}
