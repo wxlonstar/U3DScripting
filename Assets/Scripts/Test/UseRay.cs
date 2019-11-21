cf93e3fa511dc87e77c1a6cfd801be22375278e6
朱磊 <zhulei@xmfunny.com>
2019-11-21 20:02:01 +0800

    TextureAjustment

---
+++ Assets/Scripts/Test/UseRay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseRay : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Ray r = new Ray();
+        r.direction = new Vector3(0, 1, 0);
+        RaycastHit rh;
+        Physics.Raycast(r, out rh);
+        Debug.Log(rh.point);
+    }
+}
