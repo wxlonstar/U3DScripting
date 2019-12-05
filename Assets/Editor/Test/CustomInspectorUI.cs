6c897887abe6e99b62e32b878586d12e0df73d0b
zhulei <zhulei@xmfunny.com>
2019-12-06 00:13:28 +0800

    I made a CustomInspector example

---
+++ Assets/Editor/Test/CustomInspectorUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(UsingCustomInspector))]
+public class CustomInspectorUI : Editor {
+    public override void OnInspectorGUI() {
+        //base.OnInspectorGUI();
+        //GUILayout.Label("Let me see");
+        EditorGUILayout.LabelField("Current exp");
+        //UsingCustomInspector uci = (UsingCustomInspector)target;
+        UsingCustomInspector uci = target as UsingCustomInspector;
+        uci.Exp = EditorGUILayout.IntField("Exp: ", uci.Exp);
+        EditorGUILayout.LabelField("Level: " + uci.Level);
+    }
+}
---
+++ Assets/Scripts/Test/UsingCustomInspector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingCustomInspector : MonoBehaviour {
+    private int exp;
+    public int Exp {
+        set {
+            if(value >= 0) {
+                exp = value;
+            }else {
+                exp = 0;
+            }
+        }
+        get {
+            return exp;
+        }
+    }
+    public int Level {
+        get {
+            return (int)exp / 70;
+        }
+    }
+}
