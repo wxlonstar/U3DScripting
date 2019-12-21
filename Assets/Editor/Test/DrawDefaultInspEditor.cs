7d38b16d1ecde4b19068c9b599435bedb159b367
zhulei <zhulei@xmfunny.com>
2019-12-21 17:03:56 +0800

    DrawDefaultOnInspector OnInspectorGUI

---
+++ Assets/Editor/Test/DrawDefaultInspEditor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UseDrawDefaultInsp))]
+public class DrawDefaultInspEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+        EditorGUILayout.HelpBox("You wazzup", MessageType.Info);
+    }
+}
---
+++ Assets/Scripts/Test/UseDrawDefaultInsp.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseDrawDefaultInsp : MonoBehaviour {
+    
+    public Vector3 vec;
+    [Range(0, 40)]
+    public float f;
+}
