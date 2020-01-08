baec8d120884e2a89840d452ba0ce9e31d0aa4c7
zhulei <zhulei@xmfunny.com>
2020-01-09 00:32:31 +0800

    Camera Controller Editing

---
+++ Assets/Scripts/Test/CamController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour {
+    Camera cam;
+    // Start is called before the first frame update
+    void Start() {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(cam != null) {
+            CamDir(cam);
+            CamOfst(cam);
+        }
+        
+    }
+
+    void CamDir(Camera cam) {
+        float hor = Input.GetAxis("Horizontal") * Time.deltaTime ;
+        float ver = Input.GetAxis("Vertical") * Time.deltaTime;
+        Vector3 ToDirection = new Vector3(hor, ver, 0);
+        Vector3 CurrentDirection = this.transform.forward;
+        CurrentDirection += ToDirection;
+        this.transform.rotation = Quaternion.LookRotation(CurrentDirection, Vector3.up);
+      
+    }
+
+    void CamOfst(Camera cam) {
+
+    }
+}
