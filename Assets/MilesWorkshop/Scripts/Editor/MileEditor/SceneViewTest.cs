6466399cac0cf80959098c513e1c662f07333606
朱磊 <zhulei@xmfunny.com>
2020-05-20 16:26:26 +0800

    [misc] uptodate

--- Assets/MilesWorkshop/Scripts/Editor/MileEditor/SceneViewTest.cs
+++ Assets/MilesWorkshop/Scripts/Editor/MileEditor/SceneViewTest.cs
@@ -7,11 +7,16 @@ namespace MileCode {
     public class SceneViewTest : Editor {
         [MenuItem("MileTool/SceneView01")]
         public static void fun1() {
-            DrawCameraMode drawMode = DrawCameraMode.Wireframe;
+            //DrawCameraMode drawMode = DrawCameraMode.Wireframe;
             //SceneView.lastActiveSceneView.cameraMode = SceneView.GetBuiltinCameraMode(DrawCameraMode.Normal);
             SceneView sv = SceneView.lastActiveSceneView;
+            //sv.MoveToView();
+            //sv.camera.enabled = false;
+            Debug.Log(sv.camera.transform.rotation);
+            sv.camera.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            Debug.Log(sv.camera.transform.rotation);
             if(sv != null) {
-                Debug.Log(sv.cameraMode.drawMode.ToString());
+                //Debug.Log(sv.cameraMode.drawMode.ToString());
             }
         }
 
