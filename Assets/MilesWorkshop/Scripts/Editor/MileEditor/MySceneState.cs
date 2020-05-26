3046af4be8398c7fd5d6b9f1750569115a0a6bb7
朱磊 <zhulei@xmfunny.com>
2020-05-26 14:15:32 +0800

    [add] lux urp

---
+++ Assets/MilesWorkshop/Scripts/Editor/MileEditor/MySceneState.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace MileCode {
+    public class MySceneState : Editor {
+        [MenuItem("MileEditor/MySceneState")]
+        public static void SetSceneState() {
+            SetDirectionalLight();
+            SetEnvironment();
+        }
+
+        static void SetDirectionalLight() {
+            var directionalLight = GameObject.Find("Directional Light");
+            if(directionalLight != null) {
+                Light light = directionalLight.GetComponent<Light>();
+                light.lightmapBakeType = LightmapBakeType.Mixed;
+                //Debug.Log("Directonal Light now is " + light.lightmapBakeType.ToString());
+            }
+        }
+
+        static void SetEnvironment() {
+            LightmapEditorSettings.lightmapper = LightmapEditorSettings.Lightmapper.ProgressiveGPU;
+            LightmapEditorSettings.lightmapsMode = LightmapsMode.NonDirectional;
+            LightmapEditorSettings.bakeResolution = 20;
+            RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Skybox;
+            Debug.Log(LightmapEditorSettings.lightmapper.ToString());
+        }
+    }
+}
--- Assets/Misc/TestAnimController/Mover.cs
+++ Assets/Misc/TestAnimController/Mover.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mover : MonoBehaviour {
-    private Transform myPlayer = null;
-    public float speed = 1f;
-    // Start is called before the first frame update
-    void Awake() {
-        myPlayer = this.GetComponent<Transform>();
-    }
+namespace MileCode {
+    public class Mover : MonoBehaviour {
+        private Transform myPlayer = null;
+        public float speed = 1f;
+        // Start is called before the first frame update
+        void Awake() {
+            myPlayer = this.GetComponent<Transform>();
+        }
 
-    // Update is called once per frame
-    void Update() {
-        myPlayer.transform.position += speed * myPlayer.transform.forward * Time.deltaTime;
+        // Update is called once per frame
+        void Update() {
+            myPlayer.transform.position += speed * myPlayer.transform.forward * Time.deltaTime;
+        }
     }
 }
