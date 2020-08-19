024b662f0587a6ee7c3b02cba485d93a4b82c896
朱磊 <zhulei@xmfunny.com>
2020-08-19 22:21:56 +0800

    <misc> uptodate

---
+++ Assets/Scripts/JobTest/MainThreadTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class MainThreadTest : MonoBehaviour {
+    // setup data
+    NativeArray<float> reulst = new NativeArray<float>(1, Allocator.TempJob);
+
+    public void Fun() {
+        MyJob jobData = new MyJob();
+        jobData.a = 10;
+        jobData.b = 20;
+        jobData.result = this.reulst;
+    }
+}
---
+++ Assets/Scripts/JobTest/MyJob.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+
+public class MyJob : IJob {
+    public float a;
+    public float b;
+    public NativeArray<float> result;
+    public void Execute() {
+        result[0] = a + b;
+    }
+}
+
+public struct AddOneJob : IJob {
+    public NativeArray<float> result;
+    public void Execute() {
+        result[0] = result[0] + 1;
+    }
+}
+
