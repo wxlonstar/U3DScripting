b135dc6fb16ccfcb4bb9a8c0468cc01167f68a50
朱磊 <zhulei@xmfunny.com>
2020-08-24 11:50:34 +0800

    <add> JobSystem 的使用教程

--- Assets/Scripts/JobTest/MainThreadTest.cs
+++ Assets/Scripts/JobTest/MainThreadTest.cs
@@ -1,16 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections;
+﻿using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
 using UnityEngine;
+using Unity.Burst;
 
 public class MainThreadTest : MonoBehaviour {
-    // setup data
-    NativeArray<float> reulst = new NativeArray<float>(1, Allocator.TempJob);
+    [SerializeField]
+    private bool useJob;
 
-    public void Fun() {
-        MyJob jobData = new MyJob();
-        jobData.a = 10;
-        jobData.b = 20;
-        jobData.result = this.reulst;
+    private void Update() {
+        float startTime = Time.realtimeSinceStartup;
+        if(useJob) {
+            NativeList<JobHandle> jobHandleList = new NativeList<JobHandle>(Allocator.Temp);
+            for(int i = 0; i < 100; i++) {
+                JobHandle jobHandle = DoCommonJob();
+                jobHandleList.Add(jobHandle);
+            }
+            JobHandle.CompleteAll(jobHandleList);
+            jobHandleList.Dispose();
+        } else {
+            for(int i = 0; i < 100; i++) {
+                CommonTask();
+            }
+            
+        }
+        Debug.Log((Time.realtimeSinceStartup - startTime) * 1000f);
+    }
+
+    private void CommonTask() {
+        float value = 0f;
+        for(int i = 0; i < 50000; i++) {
+            math.exp10(math.sqrt(value));
+        }
+    }
+
+    private JobHandle DoCommonJob() {
+        CommonJob commonTaskJob = new CommonJob();
+        return commonTaskJob.Schedule();
+    }
+}
+
+[BurstCompile]
+public struct CommonJob : IJob {
+    public void Execute() {
+        float value = 0f;
+        for(int i = 0; i < 50000; i++) {
+            math.exp10(math.sqrt(value));
+        }
     }
 }
