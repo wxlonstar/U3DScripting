56fc68883511f980d6aaa28019d2d33a036b3bf1
Zhulei <zhulei@xmfunny.com>
2020-09-10 01:16:56 +0800

    <add> GenTexture

---
+++ Assets/Scripts/Test/MakeATexture.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[DisallowMultipleComponent]
+public class MakeATexture : MonoBehaviour {
+    //public RenderTexture m_InputTexture;
+    void SaveRenderTexture() {
+
+        int width = 2;
+        int height = 2;
+        Texture2D texture = new Texture2D(width, height, TextureFormat.RGBAFloat, false);
+
+        texture.SetPixel(0, 0, Color.red);
+        texture.SetPixel(0, 1, Color.green);
+        texture.SetPixel(1, 0, Color.blue);
+        texture.SetPixel(1, 0, Color.white);
+        
+        texture.Apply();
+
+        byte[] bytes = texture.EncodeToEXR(Texture2D.EXRFlags.CompressZIP);
+        //byte[] bytes = texture.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + "/Test.exr", bytes);
+        Debug.Log(Application.dataPath);
+        Object.DestroyImmediate(texture);
+
+    }
+
+    private void Start() {
+        Debug.Log("....");
+        SaveRenderTexture();
+    }
+}
