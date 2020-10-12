913c78fa728c6fb9db15bdd7de14613f6edf50ef
朱磊 <zhulei@xmfunny.com>
2020-10-12 16:59:31 +0800

    <add> a function that order list by name

---
+++ Assets/MilesWorkshop/Scripts/ListOrderByName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ListOrderByName {
+    private List<Shader> OrderByShaderName(List<Shader> shaders) {
+        if(shaders == null || shaders.Count <= 1) {
+            return null;
+        }
+        shaders.Sort(
+                (x, y) => string.Compare(x.name, y.name)
+            );
+        var orderedShaders = shaders.OrderBy(
+                x => x.name
+            ).ToList();
+        return orderedShaders;
+    }
+}
