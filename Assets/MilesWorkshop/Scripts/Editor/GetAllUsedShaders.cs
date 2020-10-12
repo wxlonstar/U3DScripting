b79ec8ec6a9676b7c1a1cf7355034cad79ffa7d7
朱磊 <zhulei@xmfunny.com>
2020-10-12 16:59:53 +0800

    <add> a function that get all used shaders

---
+++ Assets/MilesWorkshop/Scripts/Editor/GetAllUsedShaders.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GetAllUsedShaders {
+    static List<Shader> shadersList = new List<Shader>();
+    [MenuItem("Assets/GetAllShaders")]
+    public static void GetShadersAmongSelectedMaterials() {
+        shadersList.Clear();
+        Object[] objs = Selection.objects;
+        foreach(var obj in objs) {
+            if((obj as Material) is Material) {
+                FillShaderListWithoutRepeat(obj as Material);
+            }
+        }
+        SaveShaderList(shadersList);
+        Debug.Log(shadersList.Count);
+    }
+
+    [MenuItem("Assets/UnusedShader")]
+    public static void GetUnusedShaders() {
+        ShaderListScritableObj shaderList = AssetDatabase.LoadAssetAtPath<ShaderListScritableObj>("Assets/MileStudio/ShaderList.asset");
+        Object[] objs = Selection.objects;
+
+    }
+
+    static void SaveShaderList(List<Shader> input) {
+        //AssetDatabase.CreateAsset(new ShaderList(), Application.dataPath + "/MileStudio/shaderList.asset");
+        //ShaderList shaderList = AssetDatabase.LoadAssetAtPath<ShaderList>("Assets/MileStudio/ShaderList.asset");
+        ShaderListScritableObj shaderList_ScritableObj = new ShaderListScritableObj();
+        shaderList_ScritableObj.AddShaders(input);
+
+        //AssetDatabase.CreateAsset(shaderList_ScritableObj, "Assets/MileStudio/ShaderList_ " + System.DateTime.Now.ToString("MMddHHmmss") + ".asset");
+        AssetDatabase.CreateAsset(shaderList_ScritableObj, "Assets/MileStudio/ShaderList.asset");
+        //Debug.Log(shaderList.);
+
+    }
+
+    static void FillShaderListWithoutRepeat(Material mat) {
+        if(shadersList.Contains(mat.shader)) {
+            return;
+        } else {
+            shadersList.Add(mat.shader);
+        }
+    }
+}
---
+++ Assets/MilesWorkshop/Scripts/ShaderListScriptableObj.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+//[CreateAssetMenu(fileName = "ShaderList", menuName = "ScriptableObjects/ShaderList", order = 1)]
+public class ShaderListScritableObj : ScriptableObject {
+    public List<Shader> shaders;
+    public void AddShaders(List<Shader> shaders) {
+
+        this.shaders = OrderByShaderName(shaders);
+    }
+
+    private List<Shader> OrderByShaderName(List<Shader> shaders) {
+        if(shaders == null || shaders.Count <= 1) {
+            return null;
+        }
+        shaders.Sort(
+                (x, y) => string.Compare(x.name, y.name)
+            );
+        var orderedShaders = shaders.OrderBy(
+                x => x.name
+            ).ToList();
+        return orderedShaders;
+    }
+}
