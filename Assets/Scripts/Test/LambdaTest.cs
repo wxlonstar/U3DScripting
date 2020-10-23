25c8f07d7461240da5daa504fbe76a493ebc90f1
朱磊 <zhulei@xmfunny.com>
2020-10-23 18:53:48 +0800

    <add> fast way for readonly property

---
+++ Assets/Scripts/Test/LambdaTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LambdaTest : MonoBehaviour {
+    // Start is called before the first frame update
+    List<int> list = new List<int>();
+    void Start() {
+        
+        list[0] = 1;
+        list[1] = 2;
+        list[2] = 3;
+
+    }
+    // fast way to define readonly property
+    public int I => list.Count;
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
