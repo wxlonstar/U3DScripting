45b91584948dedf45cd32fbb98dc5db06bb2e561
朱磊 <zhulei@xmfunny.com>
2020-11-10 16:33:01 +0800

    <add> color change 脚本

---
+++ Assets/MilesWorkshop/Scripts/ColorChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Renderer))]
+public class ColorChanger : MonoBehaviour {
+    [SerializeField]
+    Color color = Color.white;
+    private new Renderer renderer;
+    private MaterialPropertyBlock propBlock;
+    // Update is called once per frame
+    private void Awake() {
+        this.renderer = GetComponent<Renderer>();
+        this.propBlock = new MaterialPropertyBlock();
+    }
+    void Update() {
+        SetColorForRobot(this.color);
+    }
+
+    private void SetColorForRobot(Color color) {
+        if(propBlock != null) {
+            Debug.Log(this.gameObject.name);
+            renderer.GetPropertyBlock(propBlock);
+            propBlock.SetColor("_BaseColor", color);
+            renderer.SetPropertyBlock(propBlock);
+        }
+    }
+}
